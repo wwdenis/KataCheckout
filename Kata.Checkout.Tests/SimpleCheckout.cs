91b1ea13c967c6c1a9fa2185272d9ed9b11e10f6
Denis Da Silva <denis81@gmail.com>
2019-02-15 07:18:48 +0100

    [Test Case] Multiple products and prices

--- Kata.Checkout.Tests/SimpleCheckout.cs
+++ Kata.Checkout.Tests/SimpleCheckout.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Tests
 {
     internal class SimpleCheckout
     {
+        readonly (string, decimal)[] prices =
+        {
+            ( "A", 50 ),
+            ( "B", 30 )
+        };
+
         readonly List<string> items = new List<string>();
 
         public SimpleCheckout()
@@ -13,7 +20,19 @@ namespace Tests
 
         internal decimal Calculate()
         {
-            return 0;
+            decimal total = 0;
+
+            // Group SKU's and count
+            var grouped = items.GroupBy(i => i).ToDictionary(k => k.Key, v => v.Count());
+
+            foreach (var group in grouped)
+            {
+                var price = prices.FirstOrDefault(i => i.Item1 == group.Key);
+                
+                total += price.Item2 * group.Value;
+            }
+
+            return total;
         }
 
         public void Scan(string sku)
