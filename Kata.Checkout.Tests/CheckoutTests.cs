61f7f059cc6c41e4720b0e0cf88d79523bbc5339
Denis Da Silva <denis81@gmail.com>
2019-02-15 07:08:49 +0100

    Dummy SimpleCheckout class

--- Kata.Checkout.Tests/CheckoutTests.cs
+++ Kata.Checkout.Tests/CheckoutTests.cs
@@ -12,7 +12,14 @@ namespace Tests
         [Test]
         public void Check_Totals()
         {
-            Assert.Fail();
+            var checkout = new SimpleCheckout();
+
+            checkout.Scan("DUMMY");
+
+            decimal total = checkout.Calculate();
+            decimal expectedTotal = 0;
+
+            Assert.AreEqual(expectedTotal, total);
         }
     }
 }
---
+++ Kata.Checkout.Tests/SimpleCheckout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tests
+{
+    internal class SimpleCheckout
+    {
+        public SimpleCheckout()
+        {
+        }
+
+        internal decimal Calculate()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Scan(string sku)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
